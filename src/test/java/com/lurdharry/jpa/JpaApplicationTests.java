f7b458aea2f5e0319db37367e62fffa56b7024d0
lurdharry <azeezhammed57@gmail.com>
2025-02-04 14:21:14 +0100

    initial commit

---
+++ src/test/java/com/lurdharry/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lurdharry.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
