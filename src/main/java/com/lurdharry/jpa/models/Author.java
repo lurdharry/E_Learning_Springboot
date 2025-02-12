d08e6ff41ef3431c3cd978a09f74030b52f5718e
lurdharry <azeezhammed57@gmail.com>
2025-02-12 22:56:19 +0100

    add identifiers

--- src/main/java/com/lurdharry/jpa/models/Author.java
+++ src/main/java/com/lurdharry/jpa/models/Author.java
@@ -1,24 +1,64 @@
 package com.lurdharry.jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Table(
+        name = "AUTHOR_DBL"
+)
 public class Author {
 
     @Id
+    @GeneratedValue
+//            (
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "author_id_gen"
+//    )
+//    @SequenceGenerator(
+//            name = "author_sequence",
+//            sequenceName = "author_sequence",
+//            allocationSize = 1
+//    )
+//    @TableGenerator(
+//            name = "author_id_gen",
+//            table = "id_generator",
+//            pkColumnName = "id_name",
+//            valueColumnName = "id_value",
+//            allocationSize = 1
+//    )
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private Integer age;
+
+    @Column(
+            updatable = false,
+             nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 
 }
