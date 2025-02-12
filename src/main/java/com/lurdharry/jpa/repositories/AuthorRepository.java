58cac3edcc2793eff477e85eaeb1a2dd73845409
lurdharry <azeezhammed57@gmail.com>
2025-02-12 23:03:00 +0100

    add author repository

---
+++ src/main/java/com/lurdharry/jpa/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.lurdharry.jpa.repositories;
+
+import com.lurdharry.jpa.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
